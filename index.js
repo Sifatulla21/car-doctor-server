8c9067d78b06c390cdf80d6c712a73df8d9394ce
Sifatulla21 <sifat2109@gmail.com>
2023-05-18 19:00:25 +0600

    update operation

--- index.js
+++ index.js
@@ -40,11 +40,38 @@ async function run() {
         const result = await serviceCollection.findOne(query, options);
         res.send(result);
     })
+    app.get('/bookings',async(req,res)=>{
+      let query = {};
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    })
     app.post('/bookings', async(req,res) => {
         const booking = req.body;
         const result = await bookingCollection.insertOne(booking);
         res.send(result);
     });
+
+    app.patch('/bookings/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedBooking = req.body;
+      const updateDoc ={
+        $set: {
+          status: updatedBooking.status
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+    app.delete('/bookings/:id',async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
